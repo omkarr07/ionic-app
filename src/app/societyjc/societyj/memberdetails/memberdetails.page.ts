1188103d955f20a6c6f3e05e5a655b8a298f3b6e
omkarr07 <khandesheomkar@gmail.com>
2020-09-01 02:19:48 +0530

    update“

--- src/app/societyjc/societyj/memberdetails/memberdetails.page.ts
+++ src/app/societyjc/societyj/memberdetails/memberdetails.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { FormGroup, FormArray, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-memberdetails',
@@ -8,11 +9,44 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['./memberdetails.page.scss'],
 })
 export class MemberdetailsPage implements OnInit {
-
-  constructor(private menuCtrl: MenuController,private sb:StatusBar) {
+ 
+  constructor(private menuCtrl: MenuController,private sb:StatusBar,private fb:FormBuilder) {
     this.sb.backgroundColorByHexString("#3880ff");
-    this.menuCtrl.enable(false);}
+    this.menuCtrl.enable(false);
+   
+    this.productForm = this.fb.group({
+      name: '',
+      quantities: this.fb.array([]) ,
+    });
+  
+  }
   ngOnInit() {
   }
+ 
+  
+  
+  productForm: FormGroup;
+  
+  quantities() : FormArray {
+    return this.productForm.get("quantities") as FormArray
+  }
+   
+  newQuantity(): FormGroup {
+    return this.fb.group({
+      qty: '',
+      vno: '',
+    })
+  }
+   
+  addQuantity() {
+    this.quantities().push(this.newQuantity());
+  }
+   
+  removeQuantity(i:number) {
+    this.quantities().removeAt(i);
+  }
+   
+ 
+
 
 }
